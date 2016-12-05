9f19d5966536771d704369d423a34f31622b3a6c
Rob Sam <robbu@live.nl>
2016-12-05 09:02:31 -0500

    fixed error contatc form

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205113402) do
+ActiveRecord::Schema.define(version: 20161205124215) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
