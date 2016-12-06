918161cf882e79ea88b7add870facb3aa74075ca
Rob Sam <robbu@live.nl>
2016-12-06 04:40:46 -0500

    fixed some errorrs

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ActionMailer::Base
-default to: 'robbu@live.nl'
-
-def contact_email(name, email, body)
-  @name = name
-  @email = email
-  @body = body
-  mail(from: email, subject: 'Contact from Message')
-end
+  default to: 'rob@simplecodecasts.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
